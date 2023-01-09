6fdf07c4a89b71eec0d5d19b5b0d16397765ddec
Sator100 <82121955+sator100@users.noreply.github.com>
2023-01-09 07:38:23 -0500

    Add files via upload

---
+++ Ejercicio/ejercicio 2/Ejercicio 2.js
@@ -0,0 +1,8 @@
+const FechaNacimiento = new Date(2004,0,21);
+const Fechalibro = new Date(2011,7,16);
+
+var InformacionPersonal = [
+    "Cristian David Parroquiano Jimenez",18,true,FechaNacimiento,Libro = {titulo:"Ready player one", Autor:"Ernest Cline", Fecha:Fechalibro, url:"https://cinedemedianoche.files.wordpress.com/2017/07/cinedemedianoche1.pdf"}
+]
+
+console.log(InformacionPersonal)
