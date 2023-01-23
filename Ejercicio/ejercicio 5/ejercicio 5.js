9e778d336af8930af899772a6e3507d2127454bd
Sator100 <82121955+sator100@users.noreply.github.com>
2023-01-23 07:00:04 -0500

    Add files via upload

---
+++ Ejercicio/ejercicio 5/ejercicio 5.js
@@ -0,0 +1,15 @@
+let height_centemeters = 159;
+let height_meters = 1.59;
+let kilograms = 43.0;
+let meters_round = height_meters.toFixed();
+let kilograms_round =  kilograms.toFixed();
+let maximo = Number.MAX_VALUE;
+
+console.log(height_centemeters)
+console.log(height_meters)
+console.log(kilograms)
+console.log(meters_round)
+console.log(kilograms_round)
+console.log(maximo)
+
+
