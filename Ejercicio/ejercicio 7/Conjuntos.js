b5a3e6a46a94729f951aeeac031e1900254cc300
Sator100 <82121955+sator100@users.noreply.github.com>
2023-06-19 09:16:12 -0500

    Add files via upload

---
+++ Ejercicio/ejercicio 7/Conjuntos.js
@@ -0,0 +1,7 @@
+
+let FamiliaNuclear = new Set(["Papa-Miguel","Mama-Monica","Hermana-Sara","yo-cristian"])
+console.log(FamiliaNuclear)
+FamiliaNuclear.add("yo-cristian")
+console.log(FamiliaNuclear)
+FamiliaNuclear.add("Javascript")
+console.log(FamiliaNuclear)
