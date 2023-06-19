96346f9aad06376a8bee7e8c80e9754b21e8a94d
Sator100 <82121955+sator100@users.noreply.github.com>
2023-06-19 10:19:24 -0500

    Add files via upload

--- Ejercicio/ejercicio 7/Objetos.js
+++ Ejercicio/ejercicio 7/Objetos.js
@@ -1,31 +1,31 @@
 
-let DatosPersonales = [
-    nombre = "Cristian David",
-    apellido = "Parroquiano Jimenez",
-    edad = 19,
-    altura = "1.60",
-    eresDesarrolllador = true
-]
+let DatosPersonales = {
+    nombre : "Cristian David",
+    apellido : "Parroquiano Jimenez",
+    edad : 19,
+    altura : "1.60",
+    eresDesarrolllador : true
+}
 
-let DatosAmigo1 = [
-    nombre = "Sara Valentina",
-    apellido = "Parroquiano Jimenez",
-    edad = 15,
-    altura = "1.44",
-    eresDesarrolllador = false
-]
+let DatosAmigo1 = {
+    nombre : "Sara Valentina",
+    apellido : "Parroquiano Jimenez",
+    edad : 15,
+    altura : "1.44",
+    eresDesarrolllador : false
+}
 
-let DatosAmigo2 = [
-    nombre = "Manuel Felipe",
-    apellido = "Bernal Castellanos",
-    edad = 19,
-    altura = "1.72",
-    eresDesarrolllador = false
-]
+let DatosAmigo2 = {
+    nombre : "Manuel Felipe",
+    apellido : "Bernal Castellanos",
+    edad : 19,
+    altura : "1.72",
+    eresDesarrolllador : false
+}
 
-const MiEdad = DatosPersonales[2]
+const MiEdad = DatosPersonales.edad
 console.log(MiEdad)
 
 let ListaPersonas = [DatosPersonales,DatosAmigo1,DatosAmigo2]
-let ListaPersonasEdad = [DatosPersonales[2], DatosAmigo1[2], DatosAmigo2[2]]
+let ListaPersonasEdad = [DatosPersonales.edad, DatosAmigo1.edad, DatosAmigo2.edad]
 console.log(ListaPersonasEdad)
